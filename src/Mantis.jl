0ebb43fec1694e287cab962b2ab9c9c6286de300
dccabanas <d.costacabanas@tudelft.nl>
2024-04-20 08:15:25 +0200

    fix: Mesh.jl include

--- src/Mantis.jl
+++ src/Mantis.jl
@@ -3,7 +3,7 @@ module Mantis
 # Remember that these include statements have to be in order, so that 
 # the include that are listed later can use code from the previous ones, 
 # but not the other way around.
-include("Mesh/mesh.jl")  # Creates Module Mesh
+include("Mesh/Mesh.jl")  # Creates Module Mesh
 include("Quadrature/Quadrature.jl")  # Creates Module Quadrature
 include("FunctionSpaces/FunctionSpaces.jl")  # Creates Module FunctionSpaces
 
