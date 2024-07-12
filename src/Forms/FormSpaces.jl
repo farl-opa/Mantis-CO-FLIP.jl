ea993020e6b61775523a22eb7af1563280682c3d
Deepesh Toshniwal <localadmin@tud501648.kpn>
2024-07-12 22:39:02 +0200

    update FormSpaces according to new derivative evaluation

--- src/Forms/FormSpaces.jl
+++ src/Forms/FormSpaces.jl
@@ -135,12 +135,11 @@ function evaluate(form_space::FS, element_idx::Int, xi::NTuple{manifold_dim, Vec
     # Loop over the spaces of the each component and evaluate them 
     # Note that the indices of the basis for each component are updated
     # by the offset defined above.
-    no_derivative_key = Tuple(0 for k in 1:manifold_dim)
     for form_component_idx in 1:n_form_components
         # Evaluate the basis for the component
         local_basis, form_basis_indices[form_component_idx] = FunctionSpaces.evaluate(form_space.fem_space[form_component_idx], element_idx, xi)
 
-        local_form_basis[form_component_idx] = local_basis[no_derivative_key]
+        local_form_basis[form_component_idx] = local_basis[1][1]
 
         # Update the basis indices by adding the offset, since they are now 
         # a component among several, so the indexing needs to include all components
@@ -191,13 +190,13 @@ function evaluate_exterior_derivative(form_space::FS, element_idx::Int, xi::NTup
     for derivative_component_idx in 1:n_derivative_form_components
         # Select the derivative for this component
         first_derivative_base_key[derivative_component_idx] = 1
-        first_derivative_key = Tuple(first_derivative_base_key)
+        first_derivative_idx = FunctionSpaces._get_derivative_idx(first_derivative_base_key)
         first_derivative_base_key[derivative_component_idx] = 0
         
         # I think deepcopy is needed  because someone may make changes in place
         # somewhere outside this function and this will affect several 
         # of the components because they reuse the same matrix if no deepcopy is used
-        local_d_form_basis_eval[derivative_component_idx] = deepcopy(d_local_fem_basis[first_derivative_key])
+        local_d_form_basis_eval[derivative_component_idx] = deepcopy(d_local_fem_basis[1][first_derivative_idx])
         form_basis_indices[derivative_component_idx] = deepcopy(fem_basis_indices)
     end
 
@@ -256,7 +255,9 @@ function evaluate_exterior_derivative(form_space::FS, element_idx::Int, xi::NTup
     form_basis_indices_dξ_2 .+= dof_offset_component[2]  # the second component basis need to be offset to have the correct numbering
     # The exterior derivative is 
     # (∂α₂/∂ξ₁ - ∂α₁/∂ξ₂) dξ₁∧dξ₂
-    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_2[1, 0], -d_local_fem_basis_dξ_1[0, 1])
+    der_idx_dξ_1_2 = FunctionSpaces._get_derivative_idx([0, 1])
+    der_idx_dξ_2_1 = FunctionSpaces._get_derivative_idx([1, 0])
+    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_2[2][der_idx_dξ_2_1], -d_local_fem_basis_dξ_1[2][der_idx_dξ_1_2])
     form_basis_indices[1] = vcat(form_basis_indices_dξ_2, form_basis_indices_dξ_1)
     
     return local_d_form_basis_eval, form_basis_indices
@@ -316,13 +317,19 @@ function evaluate_exterior_derivative(form_space::FS, element_idx::Int, xi::NTup
     # The exterior derivative is 
     # (∂α₃/∂ξ₂ - ∂α₂/∂ξ₃) dξ₂∧dξ₃ + (∂α₁/∂ξ₃ - ∂α₃/∂ξ₁) dξ₃∧dξ₁ + (∂α₂/∂ξ₁ - ∂α₁/∂ξ₂) dξ₁∧dξ₂
     # First: (∂α₃/∂ξ₂ - ∂α₂/∂ξ₃) dξ₂∧dξ₃
-    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_3[0, 1, 0], -d_local_fem_basis_dξ_2[0, 0, 1])
+    der_idx_dξ_3_2 = FunctionSpaces._get_derivative_idx([0,1,0])
+    der_idx_dξ_2_3 = FunctionSpaces._get_derivative_idx([0,0,1])
+    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_3[2][der_idx_dξ_3_2], -d_local_fem_basis_dξ_2[2][der_idx_dξ_2_3])
     form_basis_indices[1] = vcat(form_basis_indices_dξ_3, form_basis_indices_dξ_2)
     # Second: (∂α₁/∂ξ₃ - ∂α₃/∂ξ₁) dξ₃∧dξ₁
-    local_d_form_basis_eval[2] = hcat(d_local_fem_basis_dξ_1[0, 0, 1], -d_local_fem_basis_dξ_3[1, 0, 0])
+    der_idx_dξ_1_3 = FunctionSpaces._get_derivative_idx([0,0,1])
+    der_idx_dξ_3_1 = FunctionSpaces._get_derivative_idx([1,0,0])
+    local_d_form_basis_eval[2] = hcat(d_local_fem_basis_dξ_1[2][der_idx_dξ_1_3], -d_local_fem_basis_dξ_3[2][der_idx_dξ_3_1])
     form_basis_indices[2] = vcat(form_basis_indices_dξ_1, form_basis_indices_dξ_3)
     # Third: (∂α₂/∂ξ₁ - ∂α₁/∂ξ₂) dξ₁∧dξ₂
-    local_d_form_basis_eval[3] = hcat(d_local_fem_basis_dξ_2[1, 0, 0], -d_local_fem_basis_dξ_1[0, 1, 0])
+    der_idx_dξ_2_1 = FunctionSpaces._get_derivative_idx([1,0,0])
+    der_idx_dξ_1_2 = FunctionSpaces._get_derivative_idx([0,1,0])
+    local_d_form_basis_eval[3] = hcat(d_local_fem_basis_dξ_2[2][der_idx_dξ_2_1], -d_local_fem_basis_dξ_1[2][der_idx_dξ_1_2])
     form_basis_indices[3] = vcat(form_basis_indices_dξ_2, form_basis_indices_dξ_1)
     
     return local_d_form_basis_eval, form_basis_indices
@@ -384,7 +391,10 @@ function evaluate_exterior_derivative(form_space::FS, element_idx::Int, xi::NTup
     # α₁ dξ₂∧dξ₃ + α₂ dξ₃∧dξ₁ + α₃ dξ₁∧dξ₂
     # The exterior derivative is 
     # (∂α₁/∂ξ₁ + ∂α₂/∂ξ₂ + ∂α₃/∂ξ₃) dξ₁∧dξ₂∧dξ₃
-    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_2_dξ_3[1, 0, 0], d_local_fem_basis_dξ_3_dξ_1[0, 1, 0], d_local_fem_basis_dξ_1_dξ_2[0, 0, 1])
+    der_idx_dξ_2_dξ_3_1 = FunctionSpaces._get_derivative_idx([1,0,0])
+    der_idx_dξ_3_dξ_1_2 = FunctionSpaces._get_derivative_idx([0,1,0])
+    der_idx_dξ_1_dξ_2_3 = FunctionSpaces._get_derivative_idx([0,0,1])
+    local_d_form_basis_eval[1] = hcat(d_local_fem_basis_dξ_2_dξ_3[1][der_idx_dξ_2_dξ_3_1], d_local_fem_basis_dξ_3_dξ_1[2][der_idx_dξ_3_dξ_1_2], d_local_fem_basis_dξ_1_dξ_2[2][der_idx_dξ_1_dξ_2_3])
     form_basis_indices[1] = vcat(form_basis_indices_dξ_2_dξ_3, form_basis_indices_dξ_3_dξ_1, form_basis_indices_dξ_1_dξ_2)
     
     return local_d_form_basis_eval, form_basis_indices
