56e0578cdd039bd52c4489798abe038b2b43f9ff
Artur Palha <artur.palha@gmail.com>
2024-07-18 09:36:14 +0200

    Adds draft inner product functions.

--- src/Forms/FormOperators.jl
+++ src/Forms/FormOperators.jl
@@ -1,5 +1,74 @@
 # Priority 1: Inner products for abstract form fields
 
+function inner_product(f1::AbstractFormExpression{domain_dim, 0}, f2::AbstractFormExpression{domain_dim, 0}, element_idx::Int, quad_rule::Quadrature.QuadratureRule{domain_dim}) where {domain_dim, form_rank}
+    xi = get_quad_nodes(quad_rule)  # Tuple of quad nodes for tensor product 
+    weights = get_quad_weights(quad_rule)  # weights: either the tensor product
+
+    f1_eval, f1_indices = Mantis.Forms.evaluate(f1, elment_idx, xi)
+    f2_eval, f2_indices = Mantis.Forms.evaluate(f2, elment_idx, xi)
+
+    n_basis_1 = size(f1_eval[1], 2)  # how many basis in f1, if field only 1 if space then the number of basis in this component
+    n_basis_2 = size(f2_eval[1], 2)  # the same as above 
+
+    A_values = zeros(n_basis_1, n_basis_2)
+    A_row_idx = zeros(n_basis_1, n_basis_2)
+    A_column_idx = zeros(n_basis_1, n_basis_2)
+
+    det_g = inv_metric(f1, element_idx, xi) # we need this functionality for both form expressions and guarantee that both share the same geometry
+
+    for f1_basis_idx = 1:n_basis_1
+        for f2_basis_idx = 1:n_basis_2
+            A_values[f1_basis_idx, f2_basis_idx] = sum(f1_eval[1][:, f1_basis_idx] .* det_g .* f2_eval[1][:, f2_basis_idx])
+            A_row_idx[f1_basis_idx, f2_basis_idx] = f1_indices[1][f1_basis_idx]
+            A_column_idx[f1_basis_idx, f2_basis_idx] = f2_indices[1][f2_basis_idx]
+        end
+    end
+
+    return 
+end
+
+# 1-forms in 2D
+function inner_product(f1::AbstractFormExpression{2, 1}, f2::AbstractFormExpression{2, 1}, element_idx::Int, quad_rule::Quadrature.QuadratureRule{2})
+    xi = get_quad_nodes(quad_rule)  # Tuple of quad nodes for tensor product 
+    weights = get_quad_weights(quad_rule)  # weights: either the tensor product
+
+    f1_eval, f1_indices = Mantis.Forms.evaluate(f1, elment_idx, xi)
+    f2_eval, f2_indices = Mantis.Forms.evaluate(f2, elment_idx, xi)
+
+    # We need to recheck this, we need the number of active basis
+    n_basis_1 = size(f1_eval[1], 2)  # how many basis in f1, if field only 1 if space then the number of basis in this component
+    n_basis_2 = size(f2_eval[1], 2)  # the same as above 
+
+    A_values = zeros(n_basis_1, n_basis_2, 2, 2)
+    A_row_idx = zeros(n_basis_1, n_basis_2, 2, 2)
+    A_column_idx = zeros(n_basis_1, n_basis_2, 2 ,2)
+
+    g, det_g = inv_metric(f1, element_idx, xi) # we need this functionality for both form expressions and guarantee that both share the same geometry
+
+    for f1_basis_idx = 1:n_basis_1
+        for f2_basis_idx = 1:n_basis_2
+            # α¹ = α¹₁dξ¹ + α¹₂dξ²
+            # β¹ = β¹₁dξ¹ + β¹₂dξ²
+            # <α¹, β²> = <α¹₁dξ¹, β¹₁dξ¹> + <α¹₁dξ¹, β¹₂dξ²> + ...
+
+            #  <α¹₁dξ¹, β¹₁dξ¹>
+            A_values[f1_basis_idx, f2_basis_idx, 1, 1] += sum(f1_eval[1][:, f1_basis_idx] .* g[:, 1, 1] .* f2_eval[1][:, f2_basis_idx])
+            A_row_idx[f1_basis_idx, f2_basis_idx, 1, 1] = f1_indices[1][f1_basis_idx]
+            A_column_idx[f1_basis_idx, f2_basis_idx, 1, 1] = f2_indices[1][f2_basis_idx]
+
+            
+            # <α¹₁dξ¹, β¹₂dξ²>
+            A_values[f1_basis_idx, f2_basis_idx, 1, 2] += sum(f1_eval[1][:, f1_basis_idx] .* g[:, 1, 2] .* f2_eval[2][:, f2_basis_idx])
+            A_row_idx[f1_basis_idx, f2_basis_idx, 1, 2] = f1_indices[1][f1_basis_idx]
+            A_column_idx[f1_basis_idx, f2_basis_idx, 1, 2] = f2_indices[2][f2_basis_idx]
+
+            # the other terms (...)
+        end
+    end
+
+    return 
+end
+
 # (0-forms, 0-forms)
 function inner_product(f1::FormSpace{domain_dim, 0, G, Tuple{F1}}, f2::FormSpace{domain_dim, 0, G, Tuple{F2}}, element_id::Int, quad_rule::Quadrature.QuadratureRule{domain_dim}) where {domain_dim, G <: Geometry.AbstractGeometry{domain_dim, codomain_dim} where {codomain_dim}, F1 <: FunctionSpaces.AbstractFunctionSpace{domain_dim}, F2 <: FunctionSpaces.AbstractFunctionSpace{domain_dim}}
 
