c1e416d7e8060e52b38b0429b824a25705d064a0
Joey <j.dekker-2@tudelft.nl>
2024-08-15 11:07:13 +0200

    Commented out the form evaluation print statements

--- src/Forms/FormExpressions.jl
+++ src/Forms/FormExpressions.jl
@@ -98,7 +98,7 @@ Evaluate a FormField at given points.
         `n_evaluation_points = n_1 * ... * n_n`, where `n_i` is the number of points in the component `i` of the tensor product Tuple.
 """
 function evaluate(form::FormField{manifold_dim, form_rank, G, FS}, element_idx::Int, xi::NTuple{manifold_dim, Vector{Float64}}) where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}, FS <: AbstractFormSpace{manifold_dim, form_rank, G}}
-    print("Evaluating $(form.label) \n")
+    #print("Evaluating $(form.label) \n")
     n_form_components = binomial(manifold_dim, form_rank)
     form_basis_eval, form_basis_indices = evaluate(form.form_space, element_idx, xi)
 
@@ -137,7 +137,7 @@ Evaluate the exterior derivative of a FormField at given points.
         `FormField` there is only one `basis`. This is done for consistency with `FormBasis`.
 """
 function evaluate_exterior_derivative(form::FormField{manifold_dim, form_rank, G, FS}, element_idx::Int, xi::NTuple{manifold_dim, Vector{Float64}}) where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}, FS <: AbstractFormSpace{manifold_dim, form_rank, G}}
-    print("Evaluating exterior derivative of $(form.label) \n")
+    #print("Evaluating exterior derivative of $(form.label) \n")
     n_form_components = binomial(manifold_dim, form_rank)
     d_form_basis_eval, form_basis_indices = evaluate_exterior_derivative(form.form_space, element_idx, xi)
     n_derivative_components = size(d_form_basis_eval, 1)
@@ -293,7 +293,7 @@ Evaluate a unary FormExpression at given points.
 - `form_eval`: Vector of length `n_form_components`, where each element is a Vector{Float64} of length `n_evaluation_points`
 """
 function evaluate(form::FormExpression{manifold_dim, form_rank, G, Tuple{F}}, element_idx::Int, xi::NTuple{manifold_dim, Vector{Float64}}) where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}, F <: AbstractFormExpression}
-    print("Evaluating: " * form.label * "\n")
+    #print("Evaluating: " * form.label * "\n")
     if form.op == "d"
         form_eval = evaluate_exterior_derivative(form.children[1], element_idx, xi)
 
@@ -322,7 +322,7 @@ Evaluate a binary FormExpression at given points.
 # Note: This function is not fully implemented yet.
 """
 function evaluate(form::FormExpression{manifold_dim, form_rank, G, Tuple{F1, F2}}, element_idx::Int, xi::NTuple{manifold_dim, Vector{Float64}}) where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}, F1 <: AbstractFormExpression, F2 <: AbstractFormExpression}
-    print("Evaluating: " * form.label * "\n")
+    #print("Evaluating: " * form.label * "\n")
     throw("∧ not imlemented yet: (form.children[1].label, form.children[2].label)")
     form_eval = 1.0
     return form_eval
