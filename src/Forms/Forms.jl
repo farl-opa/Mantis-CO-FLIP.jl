40580c48130293dc0adaceb62977b253ccb577b6
Joey <j.dekker-2@tudelft.nl>
2024-08-19 13:46:50 +0200

    Updated variable names and comments in the get_geometry function to be clearer.

--- src/Forms/Forms.jl
+++ src/Forms/Forms.jl
@@ -45,33 +45,33 @@ function get_form_rank(form::FE) where {FE <: AbstractFormExpression{manifold_di
 end
 
 @doc raw"""
-    get_geometry(form_space::FS) where {FS <: AbstractFormSpace{manifold_dim, form_rank, G}} where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}}
+    get_geometry(form::FS) where {FS <: AbstractFormExpression{manifold_dim, form_rank, G}} where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}}
 
 Returns the geometry associated with the `form_space`.
 
 # Arguments
-- `form_space::AbstractFormSpace`: The FormSpace to get the geometry of.
+- `form::AbstractFormExpression`: The Form to get the geometry of.
 """
-function get_geometry(form_space::FS) where {FS <: AbstractFormExpression{manifold_dim, form_rank, G}} where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}}
-    return form_space.geometry
+function get_geometry(form::FS) where {FS <: AbstractFormExpression{manifold_dim, form_rank, G}} where {manifold_dim, form_rank, G <: Geometry.AbstractGeometry{manifold_dim}}
+    return form.geometry
 end
 
 @doc raw"""
-    get_geometry(form_space1::FS1, form_space2::FS2) where {FS1 <: AbstractFormSpace{manifold_dim1, form_rank1, G1}, FS2 <: AbstractFormSpace{manifold_dim2, form_rank2, G2}} where {manifold_dim1, form_rank1, G1 <: Geometry.AbstractGeometry{manifold_dim1}, manifold_dim2, form_rank2, G2 <: Geometry.AbstractGeometry{manifold_dim2}}
+    get_geometry(form_1::FS1, form_2::FS2) where {FS1 <: AbstractFormExpression{manifold_dim1, form_rank1, G1}, FS2 <: AbstractFormExpression{manifold_dim2, form_rank2, G2}} where {manifold_dim1, form_rank1, G1 <: Geometry.AbstractGeometry{manifold_dim1}, manifold_dim2, form_rank2, G2 <: Geometry.AbstractGeometry{manifold_dim2}}
 
-Returns the geometry associated with `form_space1` if it is the same as `form_space2`.
+Returns the geometry associated with `form_1` if it is the same as `form_2`.
 
 # Arguments
-- `form_space1::AbstractFormSpace`: The frist FormSpace to get the geometry of.
-- `form_space2::AbstractFormSpace`: The second FormSpace to get the geometry of.
+- `form_1::AbstractFormExpression`: The frist Form to get the geometry of, if the same as `form_2`.
+- `form_2::AbstractFormExpression`: The second Form to get the geometry of, if the same as `form_1`.
 """
-function get_geometry(form_space1::FS1, form_space2::FS2) where {FS1 <: AbstractFormExpression{manifold_dim1, form_rank1, G1}, FS2 <: AbstractFormExpression{manifold_dim2, form_rank2, G2}} where {manifold_dim1, form_rank1, G1 <: Geometry.AbstractGeometry{manifold_dim1}, manifold_dim2, form_rank2, G2 <: Geometry.AbstractGeometry{manifold_dim2}}
-    if form_space1.geometry === form_space2.geometry
-        return form_space1.geometry
+function get_geometry(form_1::FS1, form_2::FS2) where {FS1 <: AbstractFormExpression{manifold_dim1, form_rank1, G1}, FS2 <: AbstractFormExpression{manifold_dim2, form_rank2, G2}} where {manifold_dim1, form_rank1, G1 <: Geometry.AbstractGeometry{manifold_dim1}, manifold_dim2, form_rank2, G2 <: Geometry.AbstractGeometry{manifold_dim2}}
+    if form_1.geometry === form_2.geometry
+        return form_1.geometry
     else
-        geom1 = get_geometry(form_space1)
+        geom1 = get_geometry(form_1)
         tpg1 = typeof(geom1)
-        geom2 = get_geometry(form_space2)
+        geom2 = get_geometry(form_2)
         tpg2 = typeof(geom2)
         msg1 = "The given forms do not have the same geometry. "
         msg2 = "The first has geometry: $geom1 of type $tpg1. "
