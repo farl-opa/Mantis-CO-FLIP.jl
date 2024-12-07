816b5f9fadc6a1da2fdd5ffe796f6f447b9187a1
Deepesh Toshniwal <localadmin@tud501648.kpn>
2024-12-07 21:53:54 +0100

    temporary commit

--- test/Assemblers/runtests.jl
+++ test/Assemblers/runtests.jl
@@ -2,9 +2,9 @@ module AssemblersTests
 
 using Test
 
-@testset "AssemblersTest" begin include("AssemblersTests.jl") end
+# @testset "AssemblersTest" begin include("AssemblersTests.jl") end
 @testset "L2ProjectionConvergenceTests" begin include("L2ProjectionConvergenceTests.jl") end
-@testset "PoissonZeroFormConvergenceTests" begin include("PoissonZeroFormConvergenceTests.jl") end
-@testset "PolarSplineL2ProjectionTest" begin include("L2ProjectionPolarSplines.jl") end
+# @testset "PoissonZeroFormConvergenceTests" begin include("PoissonZeroFormConvergenceTests.jl") end
+# @testset "PolarSplineL2ProjectionTest" begin include("L2ProjectionPolarSplines.jl") end
 
 end
