0847286c3768dc8c5dec65df7d52e5e87e469e7d
wiggly-woodchuck <d.costacabanas@tudelft.nl>
2025-01-04 09:53:32 +0000

    add module to ExteriorDerivativeTests

--- test/Forms/runtests.jl
+++ test/Forms/runtests.jl
@@ -9,4 +9,5 @@ using Test
 @testset "Hodges" begin include("HodgeTests.jl") end
 @testset "InnerProducts" begin include("InnerProductTests.jl") end
 @testset "Wedge" begin include("WedgeTests.jl") end
+
 end
