756ba69855f97ecfbb63315ef24c3cb5cb015de9
wiggly-woodchuck <d.costacabanas@tudelft.nl>
2025-01-06 11:53:42 +0000

    print for testing

--- test/Plot/GeometryPlotTests.jl
+++ test/Plot/GeometryPlotTests.jl
@@ -40,6 +40,7 @@ geom = Mantis.Geometry.FEMGeometry(S_θrϕ, geom_coeffs_θrϕ)
 # Generate the plot
 output_filename = "fem_geometry_torus_test.vtu"
 output_file = Mantis.Plot.export_path(output_directory_tree, output_filename)
+println(output_file)
 Mantis.Plot.plot(geom; vtk_filename = output_file[1:end-4], n_subcells = 1, degree = 4, ascii = false, compress = false)
 
 # Test Plotting of 3D Geometry (toroidal annulus) -------------------------------------------
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ module MantisTests
 
 using Test
 
-@testset verbose=true "Mesh" begin include("Mesh/runtests.jl") end
-@testset verbose=true "CanonicalSpaces" begin include("CanonicalSpaces/runtests.jl") end
-@testset verbose=true "Quadrature" begin include("Quadrature/runtests.jl") end
-@testset verbose=true "FiniteElementSpaces" begin include("FiniteElementSpaces/runtests.jl") end
-@testset verbose=true "Geometry" begin include("Geometry/runtests.jl") end
-@testset verbose=true "Forms" begin include("Forms/runtests.jl") end
-@testset "Assembly" begin include("Assemblers/runtests.jl") end
+#@testset verbose=true "Mesh" begin include("Mesh/runtests.jl") end
+#@testset verbose=true "CanonicalSpaces" begin include("CanonicalSpaces/runtests.jl") end
+#@testset verbose=true "Quadrature" begin include("Quadrature/runtests.jl") end
+#@testset verbose=true "FiniteElementSpaces" begin include("FiniteElementSpaces/runtests.jl") end
+#@testset verbose=true "Geometry" begin include("Geometry/runtests.jl") end
+#@testset verbose=true "Forms" begin include("Forms/runtests.jl") end
+#@testset "Assembly" begin include("Assemblers/runtests.jl") end
 @testset verbose=true "Plot" begin include("Plot/runtests.jl") end
 
 end; nothing
