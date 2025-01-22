00ebc37621d33be7b361cbe83f71830b2fb9a990
Joey <j.dekker-2@tudelft.nl>
2025-01-22 10:21:41 +0100

    Changed to relative tolerance and added the first constructor and property tests.

--- test/Geometry/CartesianGeometryTests.jl
+++ test/Geometry/CartesianGeometryTests.jl
@@ -5,38 +5,67 @@ import Mantis
 import ReadVTK
 using Test
 
-# Refer to the following file for method and variable definitions
+# Refer to the following file for method and variable definitions.
 include("GeometryTestsHelpers.jl")
 
-# Test 2D CartesianGeometry ---------------------------------------------------
+
+# Constructor, property, and getters and setters tests -------------------------------------
+# Vector{Float64} input.
+geometry = Mantis.Geometry.CartesianGeometry(
+    ([0.0, 1.0, 2.0], [0.5, 1.5, 2.5], [-0.75, 0.0, 0.25, 0.75])
+)
+@test typeof(geometry) == Mantis.Geometry.CartesianGeometry{3}
+@test geometry.breakpoints == ([0.0, 1.0, 2.0], [0.5, 1.5, 2.5], [-0.75, 0.0, 0.25, 0.75])
+@test geometry.n_elements == (2, 2, 3)
+
+@test Mantis.Geometry.get_num_elements(geometry) == 12
+@test Mantis.Geometry.get_domain_dim(geometry) == 3
+@test Mantis.Geometry.get_image_dim(geometry) == 3
+
+# LinRange input.
+geometry = Mantis.Geometry.CartesianGeometry(
+    (LinRange(0.5, 2.5, 5), LinRange(-0.75, 0.75, 3))
+)
+@test typeof(geometry) == Mantis.Geometry.CartesianGeometry{2}
+@test geometry.breakpoints == ([0.5, 1.0, 1.5, 2.0, 2.5], [-0.75, 0.0, 0.75])
+@test geometry.n_elements == (4, 2)
+
+@test Mantis.Geometry.get_num_elements(geometry) == 8
+@test Mantis.Geometry.get_domain_dim(geometry) == 2
+@test Mantis.Geometry.get_image_dim(geometry) == 2
+
+
+
+
+# Test 2D CartesianGeometry ----------------------------------------------------------------
 for nx = 1:3
     for ny = 1:3
         geometry = Mantis.Geometry.create_cartesian_box((0.0, 0.0), (1.0, 2.0), (nx, ny))
-        
+
         # Set file name and path
         file_name = "cartesian_test_nx_$(nx)_ny_$(ny).vtu"
         output_file_path = Mantis.Plot.export_path(output_directory_tree, file_name)
-        # Generate the vtk file 
+        # Generate the vtk file
         Mantis.Plot.plot(
             geometry;
-            vtk_filename = output_file_path[1:end-4], #remove the file extension
+            vtk_filename = output_file_path[1:end-4],  # Remove the file extension.
             n_subcells = 1,
             degree = 1,
-            ascii = false, 
+            ascii = false,
             compress = false
         )
 
-        # Read the cell data from the reference file
+        # Read the cell data from the reference file.
         reference_points, reference_cells = get_point_cell_data(
             reference_directory_tree, file_name
         )
-        # Read the cell data from the output file
+        # Read the cell data from the output file.
         output_points, output_cells = get_point_cell_data(output_file_path)
-        # Check if cell data is identical
+
+        # Check if cell data is point-wise identical.
         @test all(isapprox.(reference_points, output_points; rtol=rtol))
         @test all(isequal.(reference_cells, output_cells))
     end
 end
-# -----------------------------------------------------------------------------
 
 end
