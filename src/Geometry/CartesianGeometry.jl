70e023c3e58fa0d04dd32d746ea3287516db6eab
Joey <j.dekker-2@tudelft.nl>
2025-01-22 10:17:02 +0100

    - Removed the helper functions in CartesianGeometry.jl since they were already in GeometryHelpers. - Renamed n to manifold_dim. - Changed the input type for CartesianGeometry to allow AbstractVectors of numbers, which includes for example LinRange.

--- src/Geometry/CartesianGeometry.jl
+++ src/Geometry/CartesianGeometry.jl
@@ -1,11 +1,11 @@
 import LinearAlgebra
-struct CartesianGeometry{n} <: AbstractAnalyticalGeometry{n}
-    n_elements::NTuple{n,Int}
-    breakpoints::NTuple{n,Vector{Float64}}
-    cartesian_idxs::CartesianIndices{n, NTuple{n, Base.OneTo{Int}}}
+struct CartesianGeometry{manifold_dim} <: AbstractAnalyticalGeometry{manifold_dim}
+    n_elements::NTuple{manifold_dim,Int}
+    breakpoints::NTuple{manifold_dim,Vector{Float64}}
+    cartesian_idxs::CartesianIndices{manifold_dim, NTuple{manifold_dim, Base.OneTo{Int}}}
 
     """
-        CartesianGeometry(breakpoints::NTuple{n,Vector{Float64}}) where {n}
+        CartesianGeometry(breakpoints::NTuple{manifold_dim,Vector{Float64}}) where {manifold_dim}
 
     Construct a new CartesianGeometry instance.
 
@@ -15,30 +15,30 @@ struct CartesianGeometry{n} <: AbstractAnalyticalGeometry{n}
     # Returns
     A new CartesianGeometry instance.
     """
-    function CartesianGeometry(breakpoints::NTuple{n,Vector{Float64}}) where {n}
+    function CartesianGeometry(breakpoints::NTuple{manifold_dim,T}) where {manifold_dim, N <: Number, T <: AbstractVector{N}}
         n_elements = length.(breakpoints) .- 1
         cartesian_idxs = CartesianIndices(n_elements)
-        return new{n}(n_elements, breakpoints, cartesian_idxs)
+        return new{manifold_dim}(n_elements, breakpoints, cartesian_idxs)
     end
 end
 
-function get_num_elements(geometry::CartesianGeometry{n}) where {n}
+function get_num_elements(geometry::CartesianGeometry{manifold_dim}) where {manifold_dim}
     return prod(geometry.n_elements)
 end
 
-function get_domain_dim(::CartesianGeometry{n}) where {n}
-    return n
+function get_domain_dim(::CartesianGeometry{manifold_dim}) where {manifold_dim}
+    return manifold_dim
 end
 
-function get_image_dim(::CartesianGeometry{n}) where {n}
-    return n
+function get_image_dim(::CartesianGeometry{manifold_dim}) where {manifold_dim}
+    return manifold_dim
 end
 
-function evaluate(geometry::CartesianGeometry{n}, element_idx::Int, ξ::NTuple{n,Vector{Float64}}) where {n}
+function evaluate(geometry::CartesianGeometry{manifold_dim}, element_idx::Int, ξ::NTuple{manifold_dim,Vector{Float64}}) where {manifold_dim}
 
     ordered_idx = Tuple(geometry.cartesian_idxs[element_idx])
-    univariate_points = ntuple(n) do k
-        dim_points = zeros(length(ξ[k])) # initialize vector for current dimension 
+    univariate_points = ntuple(manifold_dim) do k
+        dim_points = zeros(length(ξ[k])) # initialize vector for current dimension
 
         for point in eachindex(dim_points)
             dim_points[point] = (1-ξ[k][point])*geometry.breakpoints[k][ordered_idx[k]] +
@@ -47,115 +47,56 @@ function evaluate(geometry::CartesianGeometry{n}, element_idx::Int, ξ::NTuple{n
 
         return dim_points
     end
-    
+
     points_tensor_product_idx = CartesianIndices(size.(univariate_points, 1))  # Get the multidimensional indices for the tensor product points
 
     # Compute coordinates as the tensor product of the unidimensional points
     n_points = prod(size.(ξ, 1))  # Total number of points to evaluate
-    x = zeros(Float64, n_points, n)
+    x = zeros(Float64, n_points, manifold_dim)
     for (point_idx, point_cartesian_idx) in enumerate(points_tensor_product_idx)
-        for component_idx in 1:n 
+        for component_idx in 1:manifold_dim
             x[point_idx, component_idx] = univariate_points[component_idx][point_cartesian_idx[component_idx]]
-        end    
+        end
     end
 
     return x
 end
 
-function jacobian(geometry::CartesianGeometry{n}, element_idx::Int, ξ::NTuple{n,Vector{Float64}}) where {n}
+function jacobian(geometry::CartesianGeometry{manifold_dim}, element_idx::Int, ξ::NTuple{manifold_dim,Vector{Float64}}) where {manifold_dim}
     # Get the multi-index of the element
     ordered_idx = Tuple(geometry.cartesian_idxs[element_idx])
-    
+
     # Compute the spacing in every direction
-    dx = zeros(Float64, n)
-    for dim_idx in range(1, n)
+    dx = zeros(Float64, manifold_dim)
+    for dim_idx in range(1, manifold_dim)
         start_breakpoint_idx = ordered_idx[dim_idx]
         end_breakpoint_idx = ordered_idx[dim_idx] + 1
         dx[dim_idx] = geometry.breakpoints[dim_idx][end_breakpoint_idx] - geometry.breakpoints[dim_idx][start_breakpoint_idx]
     end
-    
+
     # Generate the Jacobian for the Cartesian grid
     # For each point, it's a diagonal matrix multiplied by the cell spacings in each direction
-    J = zeros(Float64, prod(length.(ξ)), n, n)
-    for dim_idx in range(1, n)
+    J = zeros(Float64, prod(length.(ξ)), manifold_dim, manifold_dim)
+    for dim_idx in range(1, manifold_dim)
         J[:, dim_idx, dim_idx] .= dx[dim_idx]
     end
-    
+
     return J
 end
 
-function _get_element_size(geometry::CartesianGeometry{n}, element_id::Int) where {n}
+function _get_element_size(geometry::CartesianGeometry{manifold_dim}, element_id::Int) where {manifold_dim}
     ordered_index = FunctionSpaces.linear_to_ordered_index(element_id, Vector(geometry.n_elements))
 
     element_measure = 1
-    for k ∈ 1:1:n
+    for k ∈ 1:1:manifold_dim
         element_measure *= abs(geometry.breakpoints[k][ordered_index[k]+1] - geometry.breakpoints[k][ordered_index[k]])
     end
 
     return element_measure
 end
 
-function _get_element_dimensions(geometry::CartesianGeometry{n}, element_id::Int) where {n}
+function _get_element_dimensions(geometry::CartesianGeometry{manifold_dim}, element_id::Int) where {manifold_dim}
     ordered_index = FunctionSpaces.linear_to_ordered_index(element_id, [geometry.n_elements...])
 
-    return [abs(geometry.breakpoints[k][ordered_index[k]+1] - geometry.breakpoints[k][ordered_index[k]]) for k in 1:n]
-end
-
-# Methods for ease of geometry creation
-
-@doc raw"""
-    create_cartesian_geometry(starting_point::NTuple{n, Float64}, box_size::NTuple{n, Float64}, num_elements::NTuple{n, Int}) where {n}
-
-Create a Cartesian geometry based on the provided starting point, box size, and number of elements. The geometry is created in `n`-dimensional space, where `n` is determined by the length of the input tuples.
-
-# Arguments
-- `starting_point::NTuple{n, Float64}`: The coordinates of the starting point of the geometry.
-- `box_size::NTuple{n, Float64}`: The size of the bounding box in each dimension.
-- `num_elements::NTuple{n, Int}`: The number of elements in each dimension.
-
-# Returns
-- `::CartesianGeometry{n}`: The generated Cartesian geometry structure.
-
-# Examples
-```julia
-# Create a 3D Cartesian geometry
-start_3d = (0.0, 0.0, 0.0)
-size_3d = (10.0, 5.0, 2.0)
-elements_3d = (100, 50, 20)
-geometry_3d = create_cartesian_geometry(start_3d, size_3d, elements_3d)
-```
-"""
-function create_cartesian_geometry(starting_point::NTuple{n, Float64}, box_size::NTuple{n, Float64}, num_elements::NTuple{n, Int}) where {n}
-
-    all(box_size .> 0.0) ? nothing : throw(ArgumentError("The argumente 'box_size' must be greater than 0."))
-
-    breakpoints = Tuple(collect(LinRange(starting_point[i], starting_point[i]+box_size[i], num_elements[i]+1)) for i ∈ 1:n)
-
-    return CartesianGeometry(breakpoints)
-end
-
-@doc raw"""
-    create_cartesian_geometry(starting_point::Float64, box_size::Float64, num_elements::Int)
-
-Create a 1D Cartesian geometry based on the provided starting point, box size, and number of elements. It creates a linear distribution of points from the starting point, spaced evenly across the specified box size.
-
-# Arguments
-- `starting_point::Float64`: The coordinate of the starting point of the geometry.
-- `box_size::Float64`: The size of the bounding box along the single dimension.
-- `num_elements::Int`: The number of elements along the dimension.
-
-# Returns
-- `::CartesianGeometry{1}`: The generated 1D Cartesian geometry structure.
-
-# Examples
-```julia
-# Create a 1D Cartesian geometry
-start_1d = 0.0
-size_1d = 10.0
-elements_1d = 100
-geometry_1d = create_cartesian_geometry(start_1d, size_1d, elements_1d)
-```
-"""
-function create_cartesian_geometry(starting_point::Float64, box_size::Float64, num_elements::Int)
-    return create_cartesian_geometry((starting_point,), (box_size,), (num_elements,))
+    return [abs(geometry.breakpoints[k][ordered_index[k]+1] - geometry.breakpoints[k][ordered_index[k]]) for k in 1:manifold_dim]
 end
