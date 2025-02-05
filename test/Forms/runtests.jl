ffe235d0add68646ada9e2caec878167b8572ae2
wiggly-woodchuck <d.costacabanas@tudelft.nl>
2025-02-05 16:44:27 +0100

    Removes leftover @test_nowarn in the 3d case

--- test/Forms/runtests.jl
+++ test/Forms/runtests.jl
@@ -8,8 +8,6 @@ using Test
 @testset "FormEvaluations" begin include("FormEvaluationTests.jl") end
 @testset "Hodges" begin include("HodgeTests.jl") end
 @testset "InnerProducts" begin include("InnerProductTests.jl") end
-# TODO: The current way we handle operators is very type unstable so the wedge tests take a
-# very long time. Leaving commented out until Forms module is reworked.
-#@testset "Wedge" begin include("WedgeTests.jl") end
+@testset "Wedge" begin include("WedgeTests.jl") end
 
 end
