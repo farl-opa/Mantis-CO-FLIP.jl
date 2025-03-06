e4d0259f3d64546b16afe6445539beeec92a6c36
dccabanas <d.costacabanas@tudelft.nl>
2025-03-06 16:42:02 +0000

    Adds Hierarchical 1-form Hodge Laplacian

--- examples/HodgeLaplacian/1-Form-HierarchicalB-splines.jl
+++ examples/HodgeLaplacian/1-Form-HierarchicalB-splines.jl
@@ -1,4 +1,4 @@
-module TensorProductBsplines
+module HierarchicalBsplines
 
 import Mantis
 
@@ -10,12 +10,20 @@ include("../HelperFunctions.jl")
 ############################################################################################
 # Mesh 
 starting_point = (0.0, 0.0)
-box_size = (1.0, 1.0)
-num_elements = (2, 2) .^ 3
+box_size = (fpi, fpi) #(π, π)
+num_elements = (2, 2) .^ 3 # Ininital mesh size.
 
 # B-spline parameters
-p = (3, 3) # Polynomial degrees.
+p = (2, 2) # Polynomial degrees.
 k = p .- 1 # Regularities. (Maximally smooth B-splines.)
+
+# Hierarchical parameters.
+truncate = true # true = THB, false = HB
+simplified = false
+num_steps = 3 # Number of refinement steps.
+num_sub = (2, 2) # Number of subdivisions per dimension per step.
+θ = 0.2 # Dorfler parameter.
+Lchains = true # Decide if Lchains are added to fix inexact refinements.
 
 # Quadrature rules
 nq_assembly = p .+ 1
@@ -24,36 +32,36 @@ nq_error = nq_assembly .* 2
     Mantis.Quadrature.gauss_legendre, nq_assembly, nq_error
 )
 
+forcing_function = sinusoidal_data
+
 verbose = true # Set to true for problem information.
 export_vtk = false # Set to true to export the computed solutions.
 
 ############################################################################################
 #                                       Run problem                                        #
 ############################################################################################
-# de Rham complex 
-ℜ = Mantis.Forms.create_tensor_product_bspline_de_rham_complex(
-    starting_point, box_size, num_elements, p, k
+# Hierarchical de Rham complex 
+ℌ = Mantis.Forms.create_hierarchical_de_rham_complex(
+    starting_point, box_size, num_elements, p, k, num_sub, truncate, simplified
 )
-# Only 0- and 1-forms are needed for the 1-form Hodge Laplacian.
-ℜ⁰, ℜ¹  = ℜ[1], ℜ[2]
-
-# Geometry
-⊞ = Mantis.Forms.get_geometry(ℜ⁰)
-
-# Forcing function and analytical solutions.
-u, δu, fₑ = sinusoidal_data(1, ⊞)
 
 # Solve problem
-uₕ, δuₕ = Mantis.Assemblers.solve_one_form_hodge_laplacian(ℜ⁰, ℜ¹, ∫ₐ, fₑ)
+uₕ, δuₕ = Mantis.Assemblers.solve_one_form_hodge_laplacian(
+    ℌ, forcing_function, num_steps, θ, Lchains, ∫ₐ, ∫ₑ; verbose
+)
+
 
 ############################################################################################
 #                                      Solution data                                       #
 ############################################################################################
 if export_vtk 
     println("Exporting computed solutions to VTK...")
+    geometry = Mantis.Forms.get_geometry(uₕ)
+    num_elements = Mantis.Geometry.get_num_elements(geometry)
+    u, δu = forcing_function(1, geometry)
 
-    compt_file_name = "1-Form-HodgeLaplacian-TPBsplines-computed-p=$(p)-k=$(k)-nels=$(num_elements)"
-    exact_file_name = "1-Form-HodgeLaplacian-TPBsplines-exact-p=$(p)-k=$(k)-nels=$(num_elements)"
+    compt_file_name = "1-Form-HodgeLaplacian-HBsplines-computed-p=$(p)-k=$(k)-nels=$(num_elements)"
+    exact_file_name = "1-Form-HodgeLaplacian-HBsplines-exact-p=$(p)-k=$(k)-nels=$(num_elements)"
 
     Mantis.Plot.export_form_fields_to_vtk((uₕ, δuₕ), compt_file_name)
     Mantis.Plot.export_form_fields_to_vtk((u, δu), exact_file_name)
--- src/Assemblers/AssemblersHelpers.jl
+++ src/Assemblers/AssemblersHelpers.jl
@@ -312,7 +312,7 @@ function one_form_hodge_laplacian(inputs::WeakFormInputs, element_id::Int)
     return (A_row_idx, A_col_idx, A_elem), (b_row_idx_2, b_elem_2)
 end
 
-function solve_one_form_mixed_laplacian(zero_form, one_form, qr_assembly, forcing)
+function solve_one_form_hodge_laplacian(zero_form, one_form, qr_assembly, forcing)
     weak_form_inputs = Assemblers.WeakFormInputs(
         qr_assembly, (zero_form, one_form), (zero_form, one_form), (forcing,)
     )
@@ -326,5 +326,92 @@ function solve_one_form_mixed_laplacian(zero_form, one_form, qr_assembly, forcin
     compt_zero_form.coefficients .= sol[1:coeff_offset]
     compt_one_form.coefficients .= sol[(coeff_offset + 1):end]
 
-    return compt_zero_form, compt_one_form
+    return compt_one_form, compt_zero_form
+end
+
+function solve_one_form_hodge_laplacian(
+    complex::C,
+    forcing_function::Function,
+    num_steps::Int,
+    dorfler_parameter::Float64,
+    Lchains::Bool,
+    q_rule_assembly::Quadrature.AbstractQuadratureRule{manifold_dim},
+    q_rule_error::Quadrature.AbstractQuadratureRule{manifold_dim};
+    verbose::Bool=false,
+) where {manifold_dim, num_forms, C <: NTuple{num_forms, Forms.AbstractFormSpace}}
+    if verbose
+        println("Solving the problem on initial step...")
+    end
+
+    # Exact solution on initial step
+    exact_one_form, exact_zero_form, forcing = forcing_function(
+        1, Forms.get_geometry(complex[1])
+    )
+
+    # Solve problem on initial step
+    compt_one_form, compt_zero_form = solve_one_form_hodge_laplacian(
+        complex[1], complex[2], q_rule_assembly, forcing
+    )
+
+    # Calculate element-wise error
+    err_per_element = Analysis.compute_error_per_element(
+        compt_one_form, exact_one_form, q_rule_error
+    )
+
+    for step in 1:num_steps
+        if verbose 
+            println("Solving the problem on step $step...")
+        end
+
+        zero_form_space = FunctionSpaces.get_component_spaces(complex[1].fem_space)[1]
+
+        L = FunctionSpaces.get_num_levels(zero_form_space)
+
+        new_operator, new_space = FunctionSpaces.build_two_scale_operator(
+            FunctionSpaces.get_space(zero_form_space, L),
+            FunctionSpaces.get_num_subdivisions(zero_form_space),
+        )
+
+        dorfler_marking = FunctionSpaces.get_dorfler_marking(
+            err_per_element, dorfler_parameter
+        )
+
+        # Get domains to be refined in current step
+        marked_elements_per_level = FunctionSpaces.get_padding_per_level(
+            zero_form_space, dorfler_marking
+        )
+
+        # Add Lchains if needed
+        if Lchains
+            FunctionSpaces.add_Lchains_supports!(
+                marked_elements_per_level, zero_form_space, new_operator
+            )
+        end
+        # Get children of marked elements
+        refinement_domains = FunctionSpaces.get_refinement_domains(
+            zero_form_space, marked_elements_per_level, new_operator
+        )
+
+        # Update the hierarchical complex based on the refinement domains and the 0-form space
+        complex = Forms.update_hierarchical_de_rham_complex(
+            complex, refinement_domains, new_operator, new_space
+        )
+
+        # Update exact solution
+        exact_one_form, exact_zero_form, forcing = forcing_function(
+            1, Forms.get_geometry(complex[1])
+        )
+
+        # Solve problem on current step
+        compt_one_form, compt_zero_form = solve_one_form_hodge_laplacian(
+            complex[1], complex[2], q_rule_assembly, forcing
+        )
+
+        # Calculate element-wise error
+        err_per_element = Analysis.compute_error_per_element(
+            compt_one_form, exact_one_form, q_rule_error
+        )
+    end
+
+    return compt_one_form, compt_zero_form
 end
