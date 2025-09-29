2506a20b79449f9847bfc6989b28d4d22003934f
Diogo Cabanas <dccabanas-dev@tuta.com>
2025-09-29 10:53:45 +0200

    Adds Algebraic Operators (deprecates Basis Operators)

--- test/Forms/runtests.jl
+++ test/Forms/runtests.jl
@@ -4,10 +4,23 @@ module FormTests
 
 using Test
 
-@testset "ExteriorDerivatives" begin include("ExteriorDerivativeTests.jl") end
-@testset "FormEvaluations" begin include("FormEvaluationTests.jl") end
-@testset "Hodges" begin include("HodgeTests.jl") end
-@testset "Wedge" begin include("WedgeTests.jl") end
-@testset "Integral" begin include("IntegralTests.jl") end
+@testset "ExteriorDerivatives" begin
+    include("ExteriorDerivativeTests.jl")
+end
+@testset "FormEvaluations" begin
+    include("FormEvaluationTests.jl")
+end
+@testset "Hodges" begin
+    include("HodgeTests.jl")
+end
+@testset "Wedge" begin
+    include("WedgeTests.jl")
+end
+@testset "Integral" begin
+    include("IntegralTests.jl")
+end
+@testset "Algebraic" begin
+    include("FormOperators/Algebraic.jl")
+end
 
 end
