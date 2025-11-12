2ab2d9026f386ef268a0a9873f99cb220084e132
Joey Dekker <J.Dekker-2@tudelft.nl>
2025-11-12 14:53:55 +0100

    Added benchmarks for Forms.

---
+++ benchmarks/Forms/FormExpressions/FormFields/runbenchmarks.jl
@@ -0,0 +1,31 @@
+module FormFieldsBenchmarks
+
+include(joinpath(pwd(), "BenchmarkHelpers.jl"))
+
+verbose = true
+show = true
+save = true
+
+group = BenchmarkGroup()
+
+# Module-level benchmark groups
+if verbose
+    println("Generating benchmarkable objects...")
+end
+
+mod = include("FormFields.jl")
+sub_group = getproperty(mod, :group)
+group["FormFields"] = sub_group
+
+if verbose
+    println("Done!")
+end
+
+if show || save
+    for (file_name, benchmark) in group["FormFields"]
+        file_location = relpath(dirname(@__FILE__), pwd())
+        run_benchmark(benchmark, file_location, file_name; show=show, save=save, rtol=rtol)
+    end
+end
+
+end
